1afc0c3ad5a7e91a7e5888e87d2caab02e7e8353
Andrew <Andrew.Martin2@fdmgroup.com>
2020-04-03 10:09:56 +1030

    Edit player controller for frontend

---
+++ src/main/java/com/fdm/controller/EditPlayerController.java
@@ -0,0 +1,40 @@
+package com.fdm.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fdm.dal.AccountRepository;
+import com.fdm.dal.ActorRepository;
+import com.fdm.model.Account;
+import com.fdm.model.PlayerCharacter;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api")
+public class EditPlayerController {
+	@Autowired
+	ActorRepository aRepo;
+	
+	
+
+	@PostMapping("/editAccount")
+	public PlayerCharacter postLogin(@RequestBody PlayerCharacter pc ) {
+		
+		System.out.println("/editAccount called with player character:" + pc);
+		if (pc.equals(null)) {
+			return null;
+		} else {
+			pc = aRepo.save(pc);
+			
+			return pc;
+		}
+	}
+	
+	
+}
