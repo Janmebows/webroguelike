492e0d6f197eca582c4ada2a550a7b5c7f0f3ad2
KristenEmery <60803602+KristenEmery@users.noreply.github.com>
2020-04-03 11:05:50 +1100

    simple chat

---
+++ src/main/java/com/fdm/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.fdm.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.fdm.model.Message;
+import com.fdm.model.OutputMessage;
+
+@Controller
+public class MessageController {
+	
+	@MessageMapping("/chat")
+	@SendTo("/topic/chat")
+	public OutputMessage send(Message message) throws Exception {
+		System.out.println("hi");
+		  String time = new SimpleDateFormat("HH:mm").format(new Date());
+		  return new OutputMessage(message.getFrom(), message.getText(), time);
+	}
+
+}
