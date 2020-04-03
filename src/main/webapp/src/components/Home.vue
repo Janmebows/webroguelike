77eb51921f7aca63b2d8518428952d44028fb488
Liam Mounser <liam.mounser@fdmgroup.com>
2020-04-03 13:56:55 +1100

    minor cleanup3

--- src/main/webapp/src/components/Home.vue
+++ src/main/webapp/src/components/Home.vue
@@ -25,34 +25,29 @@ export default {
       http
         .post("/home")
         .then(response => {
-          this.maps = response.data; // JSON are parsed automatically.
-          console.log(this.maps);
+          this.maps = response.data;
 
           const ROWS = 20;
           const COLS = 20;
           var map = document.getElementById("map");
-          var table = document.createElement(
-            "table"
-          ); /*Create `table` element*/
+          var table = document.createElement("table");
+
           for (var i = 0; i < ROWS; i++) {
-            var tr = document.createElement("tr"); /*Create `tr` element*/
+            var tr = document.createElement("tr");
             for (var j = 0; j < COLS; j++) {
-              var td = document.createElement("td"); /*Create `td` element*/
-              var cellText = document.createTextNode(
-                this.maps[i][j]
-              ); /*Create text for `td` element*/
-              td.appendChild(cellText); /*Append text to `td` element*/
-              tr.appendChild(td); /*Append `td` to `tr` element*/
+              var td = document.createElement("td");
+              var cellText = document.createTextNode(this.maps[i][j]);
+              td.appendChild(cellText);
+              tr.appendChild(td);
             }
-            table.appendChild(tr); /*Append `tr` to `table` element*/
+            table.appendChild(tr);
           }
           map.appendChild(table);
         })
         .catch(e => {
-          console.log("Post /home Error" + e);
+          console.log("Post /home Error, " + e);
         });
     }
-    /* eslint-enable no-console */
   },
   mounted() {
     this.retrieveMaps();
