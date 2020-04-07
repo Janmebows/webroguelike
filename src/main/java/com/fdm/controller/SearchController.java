390b703c266e52c13510c6c329d6783dcfb6e7ec
KristenEmery <60803602+KristenEmery@users.noreply.github.com>
2020-04-07 11:24:27 +1000

    fixed search controller to rest

--- src/main/java/com/fdm/controller/SearchController.java
+++ src/main/java/com/fdm/controller/SearchController.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fdm.dal.PlayerCharacterRepository;
 import com.fdm.model.PlayerCharacter;
 
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api")
 public class SearchController {
 	
 	@Autowired
